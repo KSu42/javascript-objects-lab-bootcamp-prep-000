a3772b3a48333f471376de0ec36c5d80f548b3e8
Kevin Sudor <ksudor42@gmail.com>
2018-02-22 18:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function deleteFromObjectByKey(object, key) {
   return newObject
 }
 
-function deleteFromObjectByKey
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
